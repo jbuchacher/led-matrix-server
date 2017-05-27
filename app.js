f585b7ecd5c38413b9d4aa0616909cc90705f2b5
Joshua Buchacher <jbuchacher@me.com>
2017-05-27 07:56:01 -1000

    Log socket.IO connections

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
+var SocketController = require('./controllers/SocketController');
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -29,5 +31,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.io = require('socket.io')();
+SocketController(app.io)
 
 module.exports = app;
---
+++ controllers/SocketController/index.js
@@ -0,0 +1,10 @@
+function SocketController(io) {
+  io.on('connection', function(socket){
+    console.log('a user connected')
+    socket.on('disconnect', function(){
+      console.log('user disconnected')
+    })
+  })
+}
+
+module.exports = SocketController;
